28c7d7c2d1e6c20cb54996dc73aa3007af76b348
jesse soldat <jsoldat@hotmail.com>
2016-06-30 21:28:37 +0700

    gulp file now starts a server with live reload and changes sass files to css

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,31 @@ var sass = require('gulp-sass');
 var plumber = require('gulp-plumber');
 var notify = require('gulp-notify');
 var babel = require('gulp-babel');
-
-
+var bower = require('main-bower-files');
 var concat = require('gulp-concat');
 var server = require('gulp-server-livereload');
 
-
-gulp.task('start', ['watchfiles'], function(){
-	console.log('start');
+//Default Task (gulp start)
+gulp.task('start', ['watchfiles', 'webserver'], function(){
+	
+});
+//Server with Live Reload
+gulp.task('webserver', function(){
+	return gulp.src('app')
+		.pipe(server({
+			livereload: true
+		}));
 });
 
 
+
 gulp.task('watchfiles', function() {
-	console.log('watchfiles');
-})
+	gulp.watch('./src/*.scss', ['sass']);
+});
+
+gulp.task('sass', function(){
+	
+	gulp.src('./src/*.scss')
+		.pipe( sass() )
+		.pipe( gulp.dest('./app/css') );
+});
---
+++ src/main.js
@@ -0,0 +1,1 @@
+let etsyURL = 'https://api.etsy.com/v2/listings/active.js?api_key=h9oq2yf3twf4ziejn10b717i&keywords=whiskey&includes=Images,Shop';
